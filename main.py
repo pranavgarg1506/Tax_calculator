09c2f17abf91066c555279ee1c94c892d549ba40
Pranav Garg <pranavfunny1@gmail.com>
2021-09-20 11:14:47 +0530

    Excel_functionality_added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Date- 18-09-2021
 '''
 
 import pandas as pd
+from pandas.core.reshape import reshape
 import numpy as np
 
 class Person:
@@ -12,11 +13,8 @@ class Person:
         self.name = input("Please Enter your name: ")
         self.age = int(input("Please Enter your age: "))
         self.company = input("Please ENter your company: ")
-
-    def enterInformation(self):
         self.components = ['Basic', 'HRA', 'LTA', 'City Allowance', 'Miscll', 'Non_Miscll', 'PF', 'Tax']
         self.tax_components = ['Basic', 'HRA', 'LTA', 'City Allowance', 'Miscll']
-
 
         ##  monthly salary
         self.months = {
@@ -33,6 +31,9 @@ class Person:
             10 : 'Feburary',
             11 : 'March'
         }
+
+    def enterInformation(self):
+        
         s_data = []
         for month in range(12):
             print("--------------",self.months[month],"--------------")
@@ -58,8 +59,35 @@ class Person:
 
         print("\n\nEnter Exemption Data----------------------------------------")
         self.ppf = int(input("Please Enter your contribution in PPF: "))
-        self.life_insurance = int(input("Please Enter your contribution in Life Insurance: "))
-        self.medical_premium = int(input("Please Enter your contribution in Medical Premium: "))
+        self.life_insurance = int(input("Please Enter your contribution in Life Insurance(80C): "))
+        self.medical_premium = int(input("Please Enter your contribution in Medical Premium(80D): "))
+        self.lta_cliamed = int(input("Please Enter your Claimed LTA: "))
+        self.hra_claimed = int(input("Please Enter your Claimed HRA: "))
+        self.professional_tax = int(input("Please Enter Professional Tax Deducted: "))
+
+    def downloadExcelSheet(self):
+        components = ['Basic', 'HRA', 'LTA', 'City Allowance', 'Miscll', 'Non_Miscll', 'PF', 'Tax']
+        df = pd.DataFrame(columns=components)
+        df.to_csv('Salary_breakdown.csv', index=False)
+
+    def processcsvSheet(self):
+        print("Processing the Salary BreakDown Sheet into the System....")
+        df = pd.read_csv('Salary_breakdown.csv')
+        #print(df)
+
+        s_data = df.values.tolist()
+        rows = len(s_data)
+
+        for i in range(rows, 12):
+            s_data.append(s_data[rows-1])
+                    
+        self.salary = pd.DataFrame(data = s_data, columns=self.components, index=self.months.values())
+        #print(self.salary)
+
+        print("\n\nEnter Exemption Data----------------------------------------")
+        self.ppf = int(input("Please Enter your contribution in PPF: "))
+        self.life_insurance = int(input("Please Enter your contribution in Life Insurance(80C): "))
+        self.medical_premium = int(input("Please Enter your contribution in Medical Premium(80D): "))
         self.lta_cliamed = int(input("Please Enter your Claimed LTA: "))
         self.hra_claimed = int(input("Please Enter your Claimed HRA: "))
         self.professional_tax = int(input("Please Enter Professional Tax Deducted: "))
@@ -69,36 +97,45 @@ class Person:
         figures = {}
         for col in self.salary:
             figures[col] = self.salary[col].sum()
-    
+
+        tax_dict = {}
         CTC = figures['Basic'] + figures['HRA'] + figures['LTA'] + figures['City Allowance'] + figures['Miscll'] + figures['Non_Miscll']
 
         print("TOTAL CTC", CTC)
+        tax_dict['Total CTC'] = CTC
 
         total = CTC - figures['Non_Miscll']
+        tax_dict['Taxable_Salary'] = total
 
         print("\n\nDEDUCTIONS.................................\n")
 
         ## LTA DEDUCTIONS
         print("TOTAL LTA CLAIMED YET:-",self.lta_cliamed)
+        tax_dict['LTA CLAIMED'] = self.lta_cliamed
 
         ## HRA DEDUCTIONS
         print("TOTAL HRA USED YET:-",self.hra_claimed)
+        tax_dict['HRA CLAIMED'] = self.hra_claimed
 
         ## 80C
         self.E_80C = self.ppf + self.life_insurance + figures['PF']
         print("TOTAL 80C USED YET:-",self.E_80C)
+        tax_dict['80C Deduction'] = self.E_80C
 
         ## 80D
         self.E_80D = self.medical_premium
         print("TOTAL 80D USED YET:-",self.E_80D)
+        tax_dict['80D Deduction'] = self.E_80D
 
         ## sec 16 Deductions
         self.E_16 = 50000 + self.professional_tax
         print("DEDUCTION UNDER SEC 16:-",self.E_16)
+        tax_dict['Section 16 Deduction'] = self.E_16
 
         ## GROSS TAXABLE SALARY
         tax_salary = total - self.lta_cliamed - self.hra_claimed - min(150000, self.E_80C) - self.E_80D - self.E_16
         print("\n\nGROSS TAXAXBLE SALARY",tax_salary)
+        tax_dict['GROSS TAXAXBLE SALARY'] = tax_salary
 
         ## tax has to be paid
         tax = 0
@@ -107,7 +144,7 @@ class Person:
         elif 250000 < tax_salary and tax_salary <= 500000:
             temp = tax_salary - 250000
             ans = 0.05 * temp
-            ax += ans
+            tax += ans
         elif 500000 < tax_salary and tax_salary <= 1000000:
             temp = tax_salary - 500000
             tax += 12500
@@ -124,6 +161,23 @@ class Person:
         print("TOTAL TAX TO BE PAID", tax)
         print("TOTAL TAX PAID YET", figures['Tax'])
         print("REMAINING TAX", round(tax - figures['Tax'],2))
+        tax_dict['TOTAL TAX TO BE PAID'] = tax
+        tax_dict['TOTAL TAX PAID YET'] = figures['Tax']
+        tax_dict['REMAINING TAX'] = round(tax - figures['Tax'],2)
+
+        ## Downloading the EXCEL SHeet with the TAX INFORMATION
+        file_name = 'Salary_Breakdown_'+self.name+'.csv'
+        self.salary.to_csv(file_name)
+        print("Salary BreakDown file for "+self.name+" is downoaded SUCCESSFULLY")
+
+        file_name = 'TAX_INFORMATION_'+self.name+'.csv'
+        df11 = pd.DataFrame(list(tax_dict.items()))
+        df11.to_csv(file_name,index=False,header=False)
+        print("TAX INFORMATION file for "+self.name+" is downoaded SUCCESSFULLY")
+
+
+
+
 
     def printDashboard(self):
         print("Name of the Tax Payer",self.name)
@@ -135,14 +189,25 @@ class Person:
         print("\nTotal Components: ", self.components)
         print("\nTotal Taxable Components: ", self.tax_components)
 
-
-
     def main(self):
-        self.enterInformation()
+        print("What do you want to do?")
+        print("1. Enter the informtation via program")
+        print("2. Enter data in the excel sheet and then process the data")
+        response = int(input("Enter your response  "))
+        if response == 1:
+            self.enterInformation()
+        elif response == 2:
+            res1 = input("Do you want to download the empty excel sheet(y/n)  ")
+            if res1.lower() == 'y':
+                print("\nDownloading Empty Salary BreakDown Sheet......")
+                self.downloadExcelSheet()
+                print("File is Downloaded Successfully...")
+                return
+            self.processcsvSheet()
+            
         self.printDashboard()
         self.caclulateOldRegimeTax()
 
     
-
 p = Person()
 p.main()
